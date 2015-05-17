62ab62e5458a8c02afeb459c92c4c2b60abbebfc
Allan de Queiroz <allandequeiroz@gmail.com>
2015-05-17 13:52:34 -0300

    A huge explanation about using this package

---
+++ README.md
@@ -0,0 +1,16 @@
+# Materialize Comments
+
+A Materialize Comments is a package without fussy. You don't have to translate anything, you don't have to cofigure
+nothing, just add and use.
+
+
+## How to install 
+allandequeiroz:materialize-comments
+
+
+## How to use
+{{> conversation commentableId=post._id}}
+
+
+## Observation
+Logged out people can read comments but can't comment.
---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: 'allandequeiroz:materialize-comments',
+  version: '0.0.1',
+  summary: 'A Materialize Comments Package without fussy',
+  git: 'https://github.com/allandequeiroz/materialize-comments',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.use(['accounts-password','underscore', 'mongo-livedata', 'templating', 'jquery', 'check', 'mrt:momentjs', 'random', 'aldeed:simple-schema']);
+  api.addFiles('');
+
+  api.addFiles(['lib/collection.js','lib/publication.js','lib/schema.js','lib/templates.html','lib/templates.js']);
+});
