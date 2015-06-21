76074c7abdd47aa1a73c9446d0eb0b961be14490
Papin Johyn <johynpapin@kiwiasso.org>
2015-06-21 19:08:06 +0200

    Add the bengott:avatar package
    The bengott:avatar package is the most used for add avatars..

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use(['accounts-password','underscore', 'mongo-livedata', 'templating', 'jquery', 'check', 'mrt:momentjs@2.1.0', 'random', 'aldeed:simple-schema@1.3.2']);
+  api.use(['accounts-password','underscore', 'mongo-livedata', 'templating', 'jquery', 'check', 'mrt:momentjs@2.1.0', 'random', 'aldeed:simple-schema@1.3.2', 'bengott:avatar']);
   api.addFiles('');
 
   api.addFiles(['lib/collection.js','lib/publication.js','lib/schema.js','lib/templates.html','lib/templates.js']);
